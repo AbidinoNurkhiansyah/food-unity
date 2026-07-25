b218f1119df383ca796a835f4732a323808d3f10
Abidino Nurkhiansyah <abidinonurkhiansyah@gmail.com>
2026-07-25 21:26:28 +0700

    feat(explore): add collapsible FilterSidebar component with aligned green styling

--- src/features/explore/index.ts
+++ src/features/explore/index.ts
@@ -2,3 +2,4 @@ export * from './components/ExploreHeader';
 export * from './components/ExploreSearch';
 export * from './components/ProductGrid';
 export * from './components/ProductDetailModal';
+export * from './components/FilterSidebar';
